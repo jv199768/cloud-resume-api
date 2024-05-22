a95a158fead07bf97b65e842cc51c8066d539c68
jv199768 <154536624+jv199768@users.noreply.github.com>
2024-05-22 17:34:27 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
     try:
-        response = s3_client.get_object(Bucket='s3demo01025025abc', Key='resume.json')
+        response = s3_client.get_object(Bucket='s3demo01025026abc', Key='resume.json')
         resume_data = json.loads(response['Body'].read().decode('utf-8'))
 
         return {
